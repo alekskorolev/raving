802ecbe9b32336d089ae283473f263ee45db4e6d
Aleksandr Korolev <mail@alesan.ru>
2015-05-15 00:58:13 +0300

    Add support unit test on build.

--- tasks/servetest.js
+++ tasks/servetest.js
@@ -1,37 +1,69 @@
 /*jslint browser: true, devel: true, node: true, nomen: true*/
 /*global  angular, $ */
-module.exports = function (config, gulp, mocha, jshint, stylish, watch) {
+module.exports = function (config, gulp, mocha, jslint, watch) {
   "use strict";
   gulp.task('lint', function () {
     return gulp.src(watch.js)
-      /**
-       * First, we pipe our files through `jshint`
-       */
-      .pipe(jshint())
-      /**
-       * Then we'll pipe the results through the
-       * `jshint-stylish` module which will output
-       * the results of `jshint` to the console in
-       * a human friendly format.
-       */
-      .pipe(jshint.reporter(stylish));
-      /**
-       * Finally, we want `jshint` to terminate the
-       * gulp task when it finds an error. This way
-       * our tests won't run unless `jshint` passes.
-       * To do this we'll pipe the results into the
-       * `fail` reporter.
-       */
-      //.pipe(jshint.reporter('fail'));
+
+      // pass your directives
+      // as an object
+      .pipe(jslint({
+        // these directives can
+        // be found in the official
+        // JSLint documentation.
+        node: true,
+        evil: true,
+        nomen: true,
+
+        // you can also set global
+        // declarations for all source
+        // files like so:
+        global: [],
+        predef: [],
+        // both ways will achieve the
+        // same result; predef will be
+        // given priority because it is
+        // promoted by JSLint
+
+        // pass in your prefered
+        // reporter like so:
+        reporter: 'default',
+        // ^ there's no need to tell gulp-jslint
+        // to use the default reporter. If there is
+        // no reporter specified, gulp-jslint will use
+        // its own.
+
+        // specifiy custom jslint edition
+        // by default, the latest edition will
+        // be used
+        edition: '2014-07-08',
+
+        // specify whether or not
+        // to show 'PASS' messages
+        // for built-in reporter
+        errorsOnly: false
+      }))
+
+      // error handling:
+      // to handle on error, simply
+      // bind yourself to the error event
+      // of the stream, and use the only
+      // argument as the error object
+      // (error instanceof Error)
+      .on('error', function (error) {
+        console.error(String(error));
+      });
   });
-  
-  gulp.task('serve-test', ['lint'], function () {
+
+  gulp.task('serve-test', function () {
     /**
      * Set `read` to false so gulp passes the file
-     * references straight to mocha without reading 
+     * references straight to mocha without reading
      * them. This will help speed things up.
      */
-    return gulp.src(watch.js, { read: false })
+    return gulp.src(watch.test, {
+      read: false
+    })
       /**
        * Let's pipe all of the JavaScript files
        * in our test folder into Mocha. Mocha
@@ -45,4 +77,5 @@ module.exports = function (config, gulp, mocha, jshint, stylish, watch) {
         })
       );
   });
+
 };
